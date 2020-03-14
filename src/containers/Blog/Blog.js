35b45be748f28a3ff8caed90ac8ccfab0e41bbbd
Max Dayton <daytonmaximus@gmail.com>
2020-03-14 12:41:21 -0600

    moved fullpost into post component so fullpost is rendered uderneath the posts

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -6,7 +6,6 @@ import { Route, NavLink, Switch } from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
-import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
     render () {
@@ -23,7 +22,7 @@ class Blog extends Component {
                                 //     color: "#fa923f",
                                 //     textDecoration: "underline"
                                 // }} 
-                            >Home</NavLink></li>
+                            >Posts</NavLink></li>
                             <li><NavLink to={{
                                 pathname: "/new-post",
                                 hash: "#submit",
@@ -34,9 +33,8 @@ class Blog extends Component {
                 </header>
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} /> */}
                 <Switch>
-                    <Route path="/" exact component={Posts} />
-                    <Route path="/new-post" component={NewPost} />
-                    <Route path="/:id" exact component={FullPost} />  
+                    <Route path="/new-post" component={NewPost} /> 
+                    <Route path="/" component={Posts} />
                 </Switch>
             </div>
         );
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Route } from 'react-router-dom'
 import Post from '../../../components/Post/Post';
 import './Posts.css';
+import FullPost from '../FullPost/FullPost';
 
 class Posts extends Component {
     state = {
@@ -45,9 +47,12 @@ class Posts extends Component {
         }
 
         return (
-            <section className="Posts">
-                    {posts}
-            </section>
+            <div>
+                <section className="Posts">
+                        {posts}
+                </section>
+                <Route path="/:id" exact component={FullPost} /> 
+            </div>
         )
     }
 };
